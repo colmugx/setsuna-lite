30734bb86ad1a363a9ca98cb6345ac7a87c883aa
colmugx <colmugx@qq.com>
2024-07-18 11:29:51 +0800

    feat: Update Tauri configuration to include system tray icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu};
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let quit_item = CustomMenuItem::new("quit".to_string(), "Quit");
+    let system_tray_menu = SystemTrayMenu::new().add_item(quit_item);
+
+    tauri::Builder::default()
+        .system_tray(SystemTray::new().with_menu(system_tray_menu))
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
